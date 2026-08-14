0702ea7fce3b469b2ca6b9f78ce56ea92e31ee23
Shivoo29 <2004skj@gmail.com>
2026-08-15 02:14:50 +0530

    implicit and explicit conversons on constructor

--- implicitConversion.cpp
+++ implicitConversion.cpp
@@ -7,6 +7,7 @@ class Entity
     private:
     string m_Name;
     int m_Age;
+    int m_roll;
 
     public:
     Entity(const string& name) : m_Name(name), m_Age(-1)
@@ -16,11 +17,25 @@ class Entity
     Entity(int age) : m_Name("Unknown"), m_Age(age) 
     {
     }
+
+    explicit Entity(int roll) : m_roll(roll)
+    {
+    }
 };
 
+void printEntity (const Entity& entity){
+    // some print function
+}
 int main()
 {
+
+    // printEntity("SKJ"); // this wont work because there will be 2 conversion and only one is allowed
+
+    printEntity(Entity("SKJ"));
+
     Entity a = Entity("Shivam");
-    Entity b = Entity(22); //Implicit Conversion
-       
+    Entity b (22); //Implicit Conversion
+
+    Entity c = Entity(21); // this is for explicit constructor because you have to call them
+
 }
